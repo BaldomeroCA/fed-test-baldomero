f8896732f23e7b2fb1eadf61fe98d4b228f924f4
MacBook <baldomero14640051@gmail.com>
2022-09-14 12:20:58 -0500

    first commit

--- src/js/dataset.js
+++ src/js/dataset.js
@@ -1,20 +1,24 @@
+
+
 var reportData = [
+    
     {
+        
         "cover": "images/cover1.jpg",
         "type": "Annual Report",
         "title": "2019 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
+                "file_size": "16 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2019 Annual Report"
+                "title": "2019 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2019 Proxy Statement"
+                "title": "2019 Proxy Statement "
             }
         ]
     },
@@ -24,73 +28,73 @@ var reportData = [
         "title": "2018 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
+                "file_size": "16 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2018 Annual Report"
+                "title": "2018 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2018 Proxy Statement"
+                "title": "2018 Proxy Statement "
             }
         ]
     },
     {
         "cover": "images/cover3.jpg",
-        "type": "Annual Report",
-        "title": "2017 Annual Report",
+        "type": "Annual Report ",
+        "title": "2017 Annual Report ",
         "documents": [
             {
-                "file_size": "16 KB",
-                "file_type": "PDF",
+                "file_size": "16 KB ",
+                "file_type": "PDF ",
                 "url": "files/test.pdf",
-                "title": "2017 Annual Report"
+                "title": "2017 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2017 Proxy Statement"
+                "title": "2017 Proxy Statement "
             }
         ]
     },
     {
         "cover": "images/cover1.jpg",
-        "type": "Annual Report",
+        "type": "Annual Report ",
         "title": "2016 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
-                "file_type": "PDF",
+                "file_size": "16 KB ",
+                "file_type": "PDF ",
                 "url": "files/test.pdf",
-                "title": "2016 Annual Report"
+                "title": "2016 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2016 Proxy Statement"
+                "title": "2016 Proxy Statement "
             }
         ]
     },
     {
         "cover": "images/cover2.jpg",
-        "type": "Annual Report",
+        "type": "Annual Report ",
         "title": "2015 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
+                "file_size": "16 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2015 Annual Report"
+                "title": "2015 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2015 Proxy Statement"
+                "title": "2015 Proxy Statement "
             }
         ]
     },
@@ -100,16 +104,16 @@ var reportData = [
         "title": "2014 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
+                "file_size": "16 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2014 Annual Report"
+                "title": "2014 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2014 Proxy Statement"
+                "title": "2014 Proxy Statement "
             }
         ]
     },
@@ -119,16 +123,16 @@ var reportData = [
         "title": "2013 Annual Report",
         "documents": [
             {
-                "file_size": "16 KB",
+                "file_size": "16 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2013 Annual Report"
+                "title": "2013 Annual Report "
             },
             {
-                "file_size": "7 KB",
+                "file_size": "7 KB ",
                 "file_type": "PDF",
                 "url": "files/test.pdf",
-                "title": "2013 Proxy Statement"
+                "title": "2013 Proxy Statement "
             }
         ]
     }
--- src/js/main.js
+++ src/js/main.js
@@ -3,24 +3,31 @@ var reportsWidget = {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
+            '<div class="fila">'+
+            '<div class="columna">'+
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
                     '</a>' +
+            
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                            '<h3 class="reports_title">'+
+                                '<a href="{{url}}" target="_blank">{{title}}</a>'+'<a href="{{url}}" target="_blank">({{file_size}}</a>' + '<a href="{{url}}" target="_blank">{{file_type}})</a>'+
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
+           
                 '</article>' +
+            '</div>'+
+            '</div>'+
             '{{/.}}'
         )
     },
 
     init: function() {
         this.renderReports(reportData || []);
+        
     },
 
     renderReports: function(reports) {
